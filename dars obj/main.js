b30a63d0871fedea7641e4088797801e303b54e0
Azamat <105981809+sayfiddinov1off@users.noreply.github.com>
2022-11-22 10:25:25 +0300

    Add files via upload

---
+++ dars obj/main.js
@@ -0,0 +1,17 @@
+// let obj = {
+//     name: "Azamat",
+//     surname: "Sayfiddinov",
+//     age: "16",
+//     favColor: ["blue", "black"],
+//     edu: ["OWA", "GlobalEdu"],
+
+// }
+// console.log(obj);
+
+
+
+
+// let num1 = prompt("birinchi sonni kiriting");
+// let num2 = prompt("ikkinchi sonni kiriting");
+// console.log(num1 % num2);
+
